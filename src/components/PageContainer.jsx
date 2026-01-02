5a7abc69f99f9caaec85031856a8fdb2a6e1d1d7
kirteeem <kirteemsharma.dev@gmail.com>
2026-01-02 15:03:51 +0530

    Remove root-level frontend files, keep frontend folder only

--- src/components/PageContainer.jsx
+++
@@ -1,9 +0,0 @@
-export default function PageContainer({ children }) {
-  return (
-    <div className="w-full px-[clamp(48px,8vw,200px)]">
-      <div className="mx-auto max-w-[1680px]">
-        {children}
-      </div>
-    </div>
-  );
-}
